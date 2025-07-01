b8d3566f8a8b67d651588ea2765fbc387968c402
Aryanpatel2001 <142809952+Aryanpatel2001@users.noreply.github.com>
2025-07-01 19:44:13 +0530

    Update useProducts.js

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/todos"
+const API_URL = "https://todo-backend-xra6.onrender.com/api/todos"
 
 export function useProducts() {
   const [todos, setTodos] = useState([])
